f59fa7c12139423323894eaffe3224b6ac9436d5
Vishv186 <vishvnagde186@gmail.com>
2026-03-10 15:44:28 +0530

    Initial backend setup for BagVerse Spring Boot project

---
+++ Backend/bagverse-backend/src/main/java/com/bagverse/bagverse_backend/BagverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bagverse.bagverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagverseBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/bagverse-backend/src/test/java/com/bagverse/bagverse_backend/BagverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagverse.bagverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
