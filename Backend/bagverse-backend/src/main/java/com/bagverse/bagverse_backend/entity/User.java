74b4386c48778e92edbadcc31a0c2546174777ec
Vishv186 <vishvnagde186@gmail.com>
2026-03-14 15:28:02 +0530

    Add User entity for BagVerse ecommerce platform

---
+++ Backend/bagverse-backend/src/main/java/com/bagverse/bagverse_backend/entity/User.java
@@ -0,0 +1,30 @@
+package com.bagverse.bagverse_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+}
