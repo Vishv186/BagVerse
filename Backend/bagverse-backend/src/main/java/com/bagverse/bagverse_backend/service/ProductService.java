746c3333863b94bd1b2e9b0a7fd19a541978c025
Vishv186 <vishvnagde186@gmail.com>
2026-03-14 12:58:35 +0530

    Add ProductService and ProductServiceImpl with business logic

---
+++ Backend/bagverse-backend/src/main/java/com/bagverse/bagverse_backend/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.bagverse.bagverse_backend.service;
+
+import com.bagverse.bagverse_backend.entity.Product;
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    Product createProduct(Product product);
+    Product updateProduct(Long id, Product product);
+    void deleteProduct(Long id);
+}
---
+++ Backend/bagverse-backend/src/main/java/com/bagverse/bagverse_backend/service/ProductServiceImp.java
@@ -0,0 +1,52 @@
+package com.bagverse.bagverse_backend.service;
+
+import com.bagverse.bagverse_backend.entity.Product;
+import com.bagverse.bagverse_backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class ProductServiceImp implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product product){
+        Product existingProduct = productRepository.findById(id).orElse(null);
+
+        if(existingProduct != null){
+            existingProduct.setName(product.getName());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setImageUrl(product.getImageUrl());
+            existingProduct.setStock((product.getStock()));
+            existingProduct.setCategory(product.getCategory());
+
+            return productRepository.save(existingProduct);
+        }
+        return null;
+    }
+    @Override
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+}
